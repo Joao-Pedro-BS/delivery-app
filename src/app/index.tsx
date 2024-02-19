58cb5873d826ac2497a58a14a6cf8e34c81a1cbc
Joao-Pedro-BS <134407785+Joao-Pedro-BS@users.noreply.github.com>
2024-02-19 00:34:05 -0300

    Commit inicial

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+// importação de componentes padrões
+import {View, Text} from "react-native"
+// importação de fontes
+import {Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto" 
+
+// função da Home Page
+export default function Home(){
+    // toda função retorna a visualização dos componentes
+    return(
+        <View className="pt-5 pl-5">
+            <Text className='text-white text-2xl'>React Native</Text>
+        </View>
+        
+    )
+}
