2d4211523e5962e5083f2c1f295924a287a9d3ea
Joao-Pedro-BS <134407785+Joao-Pedro-BS@users.noreply.github.com>
2024-02-21 18:07:38 -0300

    Novo componente

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,12 +3,21 @@ import {View, Text} from "react-native"
 // componente Header
 import { Header } from "@/components/header"
 
+import { CategoryBtn } from "@/components/category-btn"
+
 // função da Home Page
 export default function Home(){
     // toda função retorna a visualização dos componentes
     return(
         <View className="pt-5 pl-5">
             <Header title="Cardápio" carQuantityItens={12}/>
+            
+            <View className="flex-row gap-2 mt-0">
+                <CategoryBtn title="Oferta do Dia"/>
+                <CategoryBtn title="Entrega Grátis" isSelected/>
+                <CategoryBtn title="Perto de você"/>
+            </View>
+       
         </View>
         
     )
---
+++ src/components/category-btn.tsx
@@ -0,0 +1,15 @@
+import { Text, Pressable, PressableProps } from "react-native";
+import { clsx } from "clsx";
+
+type CategoryProps= PressableProps & {
+title: string;
+isSelected?: boolean
+}
+
+export function CategoryBtn({title, isSelected, ...rest }:CategoryProps){
+    return(
+        <Pressable className={clsx("bg-lime-100 px-4 justify-center rounded-md h-10", isSelected && "border-4 border-y-slate-500")} {...rest}>
+            <Text className="text-slate-900 font-bold ">{title}</Text>
+        </Pressable>
+    )
+}
