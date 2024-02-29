ee194878b2ddb3d4087e9a675f58429cb71340d5
Joao-Pedro-BS <134407785+Joao-Pedro-BS@users.noreply.github.com>
2024-02-29 17:05:34 -0300

    Comentários no componente Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,25 @@
-// importação de componentes padrões
+// Image -> componente para exibir imagens / View -> funciona como uma <div> /
+// Text -> componente para exibir texto / TouchableOpacity -> semelhante a um botão
 import { Image, View, Text, TouchableOpacity } from "react-native";
-// icones
+// Feather -> pacote de icones do Expo
 import { Feather } from '@expo/vector-icons';
-// cores
+// colors -> pacote de cores do Tailwind
 import  colors from 'tailwindcss/colors'
-// Tipagem
+// definindo um novo tipo chamado "HeaderProps"
+//  dentro das chaves estão os membros do tipo
 type HeaderProps={
+    // ao usar você deve passar um valor para "title", e deve ser do tipo string
     title: string;
+    // membro opcional, você escolher se passa um valor ou não
     carQuantityItens?: number;
 }
 
-// componente Header, é o nosso cabeçalho
+// Esta linha define a função Header e a exporta para que possa ser utilizada em outros arquivos.
+// A função recebe um objeto de propriedades (props) com duas propriedades
+// O : HeaderProps especifica que o objeto de props deve seguir o formato do tipo HeaderProps.
 export function Header({title, carQuantityItens=0}: HeaderProps){
     return(
+        // conteiner principal com sua estilização
         <View className="flex-row items-center border-b border-slate-50 pb-2 mx-5">
             <View className="flex-1">
                 {/* <Image source={require('@/assets/logo.png')} className="h-6 w-32" /> */}
@@ -25,7 +32,7 @@ export function Header({title, carQuantityItens=0}: HeaderProps){
                         <Text>{carQuantityItens}</Text>
                         <Feather name="shopping-bag" color={colors.black} size={20} />  
                     </View>
-                </TouchableOpacity>
+                </TouchableOpacity> 
             )}
         </View>
     )
